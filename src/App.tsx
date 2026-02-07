16965577293123e18d1b1435ec7cac8b93f1419c
jdvera96 <jdvera@espol.edu.ec>
2026-02-07 18:06:39 -0500

    desactivar modo oscuro

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ import '@ionic/react/css/text-transformation.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+/* Dark mode disabled intentionally */
 
 /* Theme variables */
 import './theme/variables.css';
